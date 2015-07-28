d4bad8e4e44dcfaa00a5e3854514505c3a75abb3
Colin Roth <colin@getwoozy.com>
2015-07-27 20:55:30 -0400

    Set up Xcode Template

---
+++ groupie/groupie/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  groupie
+//
+//  Created by Colin M. Roth on 7/27/15.
+//  Copyright (c) 2015 iamcolinroth. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ groupie/groupie/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  groupie
+//
+//  Created by Colin M. Roth on 7/27/15.
+//  Copyright (c) 2015 iamcolinroth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
